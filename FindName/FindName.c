27b631074b175a443846ff2b39f718f549f744ee
Dimesy <34558764+Dimesy@users.noreply.github.com>
2018-04-07 11:27:26 +0300

    Update FindName.c

--- FindName/FindName.c
+++ FindName/FindName.c
@@ -13,38 +13,37 @@ int main(int argc, char *argv[])
   char *splitted;
   int word_index = 0;
 
-  // Check if correct number of arguments is provided //
+  
   if(argc < 3 || argc > 3)
   {
     printf("Usage: %s <file> <string>\n", argv[0]);
     exit(1);
   }
 
-  // Check if correct extension of the file is provided //
+ 
   if(strstr(argv[1], ".txt") == NULL)
   {
     printf("Search can only be applied to txt files\n");
     return(0);
   }
   
-  // Open the file //
   if((fp = fopen(argv[1], "r")) == NULL)
   {
     printf("Error\n");
     return(0);
   }
 
-  // Loop through all lines //
+ 
   while(fgets(temp, 512, fp) != NULL)
   {
-    // Split line into words using space as a delimiter //
+    
     splitted = strtok(temp, " ");
     word_index = 0;
 
-    // Loop through all words on current line //
+    
     while((splitted != NULL))
     {
-      // Check for word containing a phraze we need //
+      
       substring = strstr(splitted, argv[2]);
 
       if(substring != NULL)
@@ -61,13 +60,13 @@ int main(int argc, char *argv[])
     line++;
   }
 
-  // Display error if no words were found //
+  
   if(!found)
   {
     printf("No matches\n");
   }
   
-  // Close the stream //
+  
   if(fp)
   {
     fclose(fp);
